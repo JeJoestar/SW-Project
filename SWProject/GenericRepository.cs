6ff7c39017d624f5ac88aa2e7fe2adbb9377da6a
JeJoestar <gekakaraza@gmail.com>
2022-02-11 14:25:28 +0200

    Removed CloneRepository and REpository interface Added GenericRepository and Unit of Work classes

---
+++ SWProject/GenericRepository.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SW.DAL
+{
+    public class GenericRepository<TEntity> where TEntity : class
+    {
+        private SWContext _swContext;
+        private readonly DbSet<TEntity> _dbSet;
+
+        public GenericRepository(SWContext context)
+        {
+            _swContext = context;
+            _dbSet = context.Set<TEntity>();
+        }
+
+        public virtual IEnumerable<TEntity> Get(
+            Expression<Func<TEntity, bool>> filter = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            string includeProperties = "")
+        {
+            IQueryable<TEntity> query = _dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            foreach (var includeProperty in includeProperties.Split
+                (new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(includeProperty);
+            }
+
+            if (orderBy != null)
+            {
+                return orderBy(query).ToList();
+            }
+            else
+            {
+                return query.ToList();
+            }
+        }
+
+        public virtual TEntity GetByID(object id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public virtual void Insert(TEntity entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public virtual void Delete(object id)
+        {
+            TEntity entityToDelete = _dbSet.Find(id);
+            Delete(entityToDelete);
+        }
+
+        public virtual void Delete(TEntity entityToDelete)
+        {
+            if (_swContext.Entry(entityToDelete).State == EntityState.Detached)
+            {
+                _dbSet.Attach(entityToDelete);
+            }
+            _dbSet.Remove(entityToDelete);
+        }
+
+        public virtual void Update(TEntity entityToUpdate)
+        {
+            _dbSet.Attach(entityToUpdate);
+            _swContext.Entry(entityToUpdate).State = EntityState.Modified;
+        }
+    }
+}
---
+++ SWProject/UnitOfWork.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SW.DAL
+{
+    public class UnitOfWork : IDisposable
+    {
+        private SWContext _swContext = new SWContext();
+        private GenericRepository<Clone> _cloneRepository;
+        private GenericRepository<Droid> _droidRepository;
+
+        public GenericRepository<Clone> CloneRepository
+        {
+            get
+            {
+
+                if (_cloneRepository == null)
+                {
+                    _cloneRepository = new GenericRepository<Clone>(_swContext);
+                }
+                return _cloneRepository;
+            }
+        }
+
+        public GenericRepository<Droid> DroidRepository
+        {
+            get
+            {
+
+                if (_droidRepository == null)
+                {
+                    _droidRepository = new GenericRepository<Droid>(_swContext);
+                }
+                return _droidRepository;
+            }
+        }
+
+        public void Save()
+        {
+            _swContext.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _swContext.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
