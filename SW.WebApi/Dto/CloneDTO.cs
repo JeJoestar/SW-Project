ee2d621227b885747073aa01d03d3741454bcbf3
yevhen.kakaraza <gekakaraza@gmail.com>
2022-03-11 13:46:43 +0200

    Updated CloneDto

--- SW.WebApi/Dto/CloneDTO.cs
+++ SW.WebApi/Dto/CloneDTO.cs
@@ -2,20 +2,14 @@
 // Copyright (c) Star Wars Inc. All rights reserved.
 // </copyright>
 
-using System.ComponentModel.DataAnnotations;
-
 namespace SW.DAL
 {
     public class CloneDto
     {
-        [Required]
-        [StringLength(4)]
         public string Number { get; set; }
-        [Required]
         public int LegionId { get; set; }
         public int? BaseId { get; set; }
         public int? StarshipId { get; set; }
-        [Required]
         public string Equipment { get; set; }
         public string Qualification { get; set; }
     }
